34641d3d61fd18b9a31196f09c8bee8c9f5d092b
lmaothisname <trancaoanhkiet@gmail.com>
2025-11-11 19:17:59 +0700

    Refactor Spotify credentials and username

--- Day 46/main.py
+++ Day 46/main.py
@@ -3,8 +3,8 @@ import spotipy
 import pprint
 from spotipy.oauth2 import SpotifyOAuth
 from bs4 import BeautifulSoup
-ID="0a196f63a4be4ae7892152e962a58e65"
-SECRET="901880ef0f40438786ee71d4eb8caee5"
+ID="ur_id"
+SECRET="ur_secret"
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
@@ -15,7 +15,7 @@ soup = BeautifulSoup(response.text, 'html.parser')
 song_names_spans = soup.select("li ul li h3")
 song_names = [song.getText().strip() for song in song_names_spans]
 
-sp =spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=ID,client_secret=SECRET,redirect_uri="https://example.com/callback",scope="playlist-modify-private",show_dialog=True,cache_path=r"D:\python\Day 46\.cache",username="wpm500w30ss6xo5p1vm9etpa6"))   
+sp =spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=ID,client_secret=SECRET,redirect_uri="https://example.com/callback",scope="playlist-modify-private",show_dialog=True,cache_path=r"D:\python\Day 46\.cache",username="ur_username))   
 user_id=sp.current_user()["id"]
 song_uris=[]
 
@@ -29,3 +29,4 @@ for song in song_names:
         print(f"{song} doesn't exist in Spotify. skipped.")
 playlist = sp.user_playlist_create(user=user_id,name=f"{date} Billboard 100",public=False)
 sp.playlist_add_items(playlist_id=playlist["id"],items=song_uris)
+
