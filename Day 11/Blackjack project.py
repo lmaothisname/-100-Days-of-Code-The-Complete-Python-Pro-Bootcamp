df0c1c11c3bfc699019b82379e025d0b14ec2617
lmaothisname <trancaoanhkiet@gmail.com>
2026-03-06 14:48:53 +0700

    Refactor Blackjack game structure and logic
    Refactor Blackjack game logic and improve card handling.

--- Day 11/Blackjack project.py
+++ Day 11/Blackjack project.py
@@ -1,4 +1,5 @@
 import random
+from os import system
 logo = r"""
 .------.            _     _            _    _            _    
 |A_  _ |.          | |   | |          | |  (_)          | |   
@@ -9,66 +10,81 @@ logo = r"""
       |  \/ K|                            _/ |                
       `------'                           |__/           
 """
+
 cards = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
 
 def draw():
-    """Take a card"""
-    return random.choice(cards)
+  """Drawn a card"""
+  return random.choice(cards)
 
-def hand_value(card):
-    """sum of their score of card"""
-    total = 0
-    ace = 0
-    for c in card:
-        if c == "A":
-            total += 11
-            ace += 1
-        elif c in ('J',"Q","K","A"):
-            total += 10
-        else:
-            total += int(c)
-    #Down value of ace from 11 to 1 if greater than 21:
-    while total > 21 and ace:
-        total -= 10
-        ace -= 1
-    return total
-
-ask_player = input("Do you want to play a game of Blackjack? type 'y' or 'n': ")
-while ask_player == "y":
-    print(logo)
-    player = [draw(),draw()]
-    dealer = [draw()]
-
-    #player turn
-    while True:
-        p_score = hand_value(player)
-        print(f"\t  Your card: {player}, current score: {p_score}")
-        print(f"\t  Computer's first card: {dealer}")
-        if p_score == 21:
-            break
-        hit = input("Type 'y' to get another card, type 'n' to pass: ")
-        if hit == "y":
-            player.append(draw())
-            if hand_value(player) > 21:
-                break
-        else:
-            break
-    
-    #computer turn, draw a card when greater than 17
-    while hand_value(dealer) < 17:
-        dealer.append(draw())
-    p_score = hand_value(player)
-    d_score = hand_value(dealer)
-    print(f"\tYour final hand: {player}, final score: {p_score}")
-    print(f"\tComputer's final hand: {dealer} , final score: {d_score}")
-    if p_score > 21:
-        print("You went over. You lose (╥﹏╥)")
-    elif d_score > 21:
-        print("Opponent went over. You win (^_^)")
-    elif p_score > d_score:
-        print("Opponent went over. You win (^_^)")
-    elif p_score < d_score:
-        print("You went over. You lose (╥﹏╥)")
+def hand_value(hand):
+  score = 0
+  ace_count = 0
+  for c in hand:
+    if c == "A":
+      score += 11
+      ace_count += 1
+    elif c in ["J","Q","K"]:
+      score += 10
     else:
-        print("Draw!")
-    ask_player = input("Do you want to play a game of Blackjack? type 'y' or 'n': ")
+      score += int(c)
+  while score > 21 and ace_count > 0:
+    score -= 10
+    ace_count -= 1
+  return score
+
+def play_game():
+  print(logo)
+  player_cards = [draw(),draw()]
+  dealer_cards = [draw(),draw()]
+
+  game_over = False
+  while not game_over:
+    player_score =  hand_value(player_cards)
+    dealer_score = hand_value(dealer_cards)
+    print(f"\tYour cards: {player_cards}, current score: {player_score}")
+    print(f"\tComputer's first card: {dealer_cards[0]}")
+    
+    if player_score == dealer_score == 21:
+      print(f"\tYour final hand: {player_cards}, final score: {player_score}")
+      print(f"\tComputer's final hand: {dealer_cards}, final_score: {dealer_score}")
+      print("It's a draw.")
+      return
+    elif player_score == 21:
+      print(f"\tYour final hand: {player_cards}, final score: {player_score}")
+      print(f"\tComputer's final hand: {dealer_cards}, final_score: {dealer_score}")
+      print("Win with a Blackjack 😎")
+      return
+    elif dealer_score == 21:
+      print(f"\tYour final hand: {player_cards}, final score: {player_score}")
+      print(f"\tComputer's final hand: {dealer_cards}, final_score: {dealer_score}")
+      print("Lose, opponent has a Blackjack 😱")
+      return
+    else:
+      hit = input("Type 'y' to get another card, type 'n' to pass:")
+      if hit == "y":
+        player_cards.append(draw())
+      else:
+        game_over = True
+  
+  while dealer_score != 0 and dealer_score < 16:
+    dealer_cards.append(draw())
+    dealer_score = hand_value(dealer_cards)
+  
+  print(f"\tYour final hand: {player_cards}, final score: {player_score}")
+  print(f"\tComputer's final hand: {dealer_cards}, final_score: {dealer_score}")
+  
+  if player_score > 21:
+    print("You went over. You lose 😭")
+  elif dealer_score > 21:
+    print("Opponent went over. You win 😁")
+  elif player_score > dealer_score:
+    print("You have higher score. You win! 😁")
+  elif player_score < dealer_score:
+    print("You have lower score. You lose! 😭")
+  else:
+    print("It's a draw! ")
+
+
+while input("Do you want to play a game of Blackjack? type 'y' or 'n': ") == "y":
+  play_game()
