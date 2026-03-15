0f13ceab3547116819228b37a9ee04c08af756b2
lmaothisname <trancaoanhkiet@gmail.com>
2026-03-15 16:00:55 +0700

    Refactor coffee machine code for clarity and efficiency

--- Day 15/Coffee Machine Project.py
+++ Day 15/Coffee Machine Project.py
@@ -29,95 +29,61 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
-def format_resources(rp):
-    resources_milk = resources["milk"]
-    resources_water = resources["water"]
-    resources_coffee = resources["coffee"]
-    return f"Water: {resources_water}ml\nMilk: {resources_milk}ml\nCoffee: {resources_coffee}g"
+money = 0
 total = 0
-def buyer_purchaser(quarters,dimes,nickles,pennies):
-    sum = 0.25 * quarters + dimes * 0.10 + nickles * 0.05 + pennies * 0.01
-    return sum
-machine_on_off = True
-def resources_insufficient_espresso():
-    if resources["water"] - MENU["espresso"]["ingredients"]["water"] < MENU["espresso"]["ingredients"]["water"]:
-        return "Sorry there is not enough water."
-    else:
-        return "Sorry there is not enough coffee."
-def resources_insufficient_latte():
-    if resources["water"] - MENU["latte"]["ingredients"]["water"] < MENU["latte"]["ingredients"]["water"]:
-        return "Sorry there is not enough water."
-    elif resources["coffee"] - MENU["latte"]["ingredients"]["coffee"] < MENU["latte"]["ingredients"]["coffee"]:
-        return "Sorry there is not enough coffee."
-    else:
-        return "Sorry there is not enough milk."
-def resources_insufficient_cappuccino():
-    if resources["water"] - MENU["cappuccino"]["ingredients"]["water"] < MENU["cappuccino"]["ingredients"]["water"]:
-        return "Sorry there is not enough water."
-    elif resources["coffee"] - MENU["cappuccino"]["ingredients"]["coffee"] < MENU["cappuccino"]["ingredients"]["coffee"]:
-        return "Sorry there is not enough coffee."
-    else:
-        return "Sorry there is not enough milk."
-# ToDo 1: create a promt "What would you like"
-while machine_on_off:
-    ask_user = input("What would you like? (espresso/latte/cappuccino): ")
-    if ask_user == "report":
-        print(format_resources(resources))
-    elif ask_user == "espresso":
-        if resources["water"] >= MENU["espresso"]["ingredients"]["water"] and resources["coffee"] >= MENU["espresso"]["ingredients"]["coffee"]:
-            print("please insert a coin ?")
-            resources["water"] = resources["water"] - MENU["espresso"]["ingredients"]["water"]
-            resources["coffee"] = resources["coffee"] - MENU["espresso"]["ingredients"]["coffee"]
-            buyer_quarters = int(input("How many quarters?: "))
-            buyer_dimes = int(input("How many dimes?: "))
-            buyer_nickles = int(input("How many nickles?: "))
-            buyer_pennies = int(input("How many pennies?: "))
-            total = buyer_purchaser(buyer_quarters,buyer_dimes,buyer_nickles,buyer_pennies)
-            if total > MENU["espresso"]["cost"]:
-                refund_money = total - MENU["espresso"]["cost"]
-                print(f"Here is ${refund_money} in change.")
-                print("Here is your espresso ☕ Enjoy!")
-            else:
-                print("Sorry that's not enough money. Money refunded.")
-        else:
-            resources_insufficient_espresso()
-    elif ask_user == "latte":
-        if resources["water"] >= MENU["latte"]["ingredients"]["water"] and resources["coffee"] >=  MENU["latte"]["ingredients"]["coffee"] and resources["milk"] >= MENU["latte"]["ingredients"]["milk"]:
-            print("please insert a coin ?")
-            resources["water"] = resources["water"] - MENU["latte"]["ingredients"]["water"]
-            resources["coffee"] = resources["coffee"] - MENU["latte"]["ingredients"]["coffee"]
-            resources["milk"] = resources["milk"] - MENU["latte"]["ingredients"]["milk"]
-            buyer_quarters = int(input("How many quarters?: "))
-            buyer_dimes = int(input("How many dimes?: "))
-            buyer_nickles = int(input("How many nickles?: "))
-            buyer_pennies = int(input("How many pennies?: "))
-            total = buyer_purchaser(buyer_quarters,buyer_dimes,buyer_nickles,buyer_pennies)
-            if total > MENU["latte"]["cost"]:
-                refund_money = total - MENU["latte"]["cost"]
-                print(f"Here is ${refund_money} in change.")
-                print("Here is your latte ☕ Enjoy!")
-            else:
-                print("Sorry that's not enough money. Money refunded.")
-        else:
-            resources_insufficient_latte()
-    elif ask_user == "cappuccino":
-        if resources["water"] >= MENU["cappuccino"]["ingredients"]["water"] and resources["coffee"] >=  MENU["cappuccino"]["ingredients"]["coffee"] and resources["milk"] >= MENU["cappuccino"]["ingredients"]["milk"]:
-            print("please insert a coin ?")
-            resources["water"] = resources["water"] - MENU["cappuccino"]["ingredients"]["water"]
-            resources["coffee"] = resources["coffee"] - MENU["cappuccino"]["ingredients"]["coffee"]
-            resources["milk"] = resources["milk"] - MENU["cappuccino"]["ingredients"]["milk"]
-            buyer_quarters = int(input("How many quarters?: "))
-            buyer_dimes = int(input("How many dimes?: "))
-            buyer_nickles = int(input("How many nickles?: "))
-            buyer_pennies = int(input("How many pennies?: "))
-            total = buyer_purchaser(buyer_quarters,buyer_dimes,buyer_nickles,buyer_pennies)
-            if total > MENU["cappuccino"]["cost"]:
-                refund_money = total - MENU["cappuccino"]["cost"]
-                print(f"Here is ${refund_money} in change.")
-                print("Here is your cappuccino ☕ Enjoy!")
-            else:
-                print("Sorry that's not enough money. Money refunded.")
-        else:
-            print(resources_insufficient_cappuccino())
-    else:
-        machine_on_off = False
+status_machine = True
+def format_resources():
+  resource_water = resources["water"]
+  resource_milk  = resources["milk"]
+  resources_coffee = resources["coffee"]
+  return f"Water: {resource_water}ml\nMilk: {resource_milk}ml\nCoffee: {resources_coffee}g\nMoney: ${total}"
+#TODO 4: Check resouces sufficient?
+def resources_insufficient(drink):
+  for item in MENU[drink]["ingredients"]:
+    if resources[item] < MENU[drink]["ingredients"][item]:
+      print(f"Sorry there is not enough {item}.")
+      return False
+  return True
+  
+def update_resource(drink):
+  for item in MENU[drink]["ingredients"]:
+    resources[item] -= MENU[drink]["ingredients"][item]
+  
+def process_coins():
+  print("Please insert coins.")
+  quarters = int(input("How many quarters?"))
+  dimes = int(input("How many dimes?"))
+  nickles = int(input("How many nickles?"))
+  pennies = int(input("How many pennies?"))
+  total_money = 0.25 * quarters + 0.1 * dimes + 0.05 * nickles + 0.01 * pennies
+  return total_money
+
+while status_machine:
+  #TODO 1: check the user's input to decice what to do next
+  user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
+  #TODO 2: Turn of the coffee machine by entering "off" to the prompt.
+  if user_input == "off":
+    status_machine  = False
+  #TODO 3: Print report.
+  elif user_input == "report":
+    print(format_resources())
+  #TODO 6: Check transaction successful?
+  elif user_input in ["espresso","latte","cappuccino"]:
+    if resources_insufficient(user_input):
+      #TODO 5: Process coins.
+      buyer_money = process_coins()
+      drink_cost = MENU[user_input]["cost"]
+      #TODO 7: Make coffee.
+      if buyer_money >= drink_cost:
+        change = round(buyer_money - drink_cost,2)
+        if change > 0:
+          print(f"Here is ${change} in change.")
+        update_resource(user_input)
+        total += drink_cost
+        print(f"Here is your {user_input} ☕ Enjoy!")
+      else:
+        print("Sorry that's not enough money. Money refunded.")
+  else:
+    print("Invalid input. Please try again.")
+
+
